100e26f454d9eb537d5efd1daefb271ea60e3374
weekendzhu <zhuweiqiang@mail.weisanyun.com>
2019-05-29 17:21:35 +0800

    更新最新可运行稳定版的springcloud

--- eureka-client/src/main/java/com/eurekaclient/controller/IndexController.java
+++ eureka-client/src/main/java/com/eurekaclient/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @Value("${server.port}")
     String port;
-    @Value("${zhf.test}")
+
     private String hello;
 
 
